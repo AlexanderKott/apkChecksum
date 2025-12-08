70cd36d5f2e15341aaf893fff2cfb52e79a40ee2
Alex Kotikov <ak-developer@yandex.ru>
2025-12-08 11:41:01 +0300

    мелкие исправления, дабавить ридми.пдф

--- app/src/main/java/com/kotikov/technicalTask/forDrWeb/presentation/Utils.kt
+++ app/src/main/java/com/kotikov/technicalTask/forDrWeb/presentation/Utils.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.core.content.FileProvider
+import com.kotikov.technicalTask.forDrWeb.BuildConfig
 import java.io.File
 
 class FileSharer(private val context: Context) {
@@ -11,7 +12,7 @@ class FileSharer(private val context: Context) {
     fun shareFile(file: File, title: String = "Отправить отчет") {
         val uri: Uri = FileProvider.getUriForFile(
             context,
-            "${context.packageName}.fileprovider",
+            BuildConfig.FILE_PROVIDER_AUTHORITY,
             file
         )
 
@@ -24,6 +25,7 @@ class FileSharer(private val context: Context) {
 
         val chooserIntent = Intent.createChooser(shareIntent, title)
         chooserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        chooserIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
 
         context.startActivity(chooserIntent)
     }
